d02351d8d4945c36d4881195dd82de0de13fc764
Christopher Mejia <christophermejia111@gmail.com>
2024-06-11 02:45:32 -0400

    now able to parse team data and insert into supabase database

--- Models/team.py
+++ Models/team.py
@@ -8,7 +8,7 @@ class Team:
                  losses,
                  goals_for,
                  goals_against,
-                 goals_diff,
+                 goal_diff,
                  attendance_per_game):
         self.team_name = team_name
         self.rank = rank,
@@ -18,6 +18,6 @@ class Team:
         self.losses = losses
         self.goals_for = goals_for
         self.goals_against = goals_against
-        self.goals_diff = goals_diff
+        self.goal_diff = goal_diff
         self.attendance_per_game = attendance_per_game
         
--- Scripts/parse.py
+++ Scripts/parse.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.environ.get("SUPABASE_ANON_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 def insert_team_stats(conf):
     team_stats = conf.find_all("tr")
@@ -9,24 +16,30 @@ def insert_team_stats(conf):
         team_name = team_stat.find("a", href=True)
         if not team_name:
             continue
+        team_name = team_name.text.strip()
+        team_rank = team_stat.find("th", attrs={"data-stat": "rank"}).text.strip()
+        team_games = team_stat.find("td", attrs={"data-stat": "games"}).text.strip()
+        team_wins = team_stat.find("td", attrs={"data-stat": "wins"}).text.strip()
+        team_ties = team_stat.find("td", attrs={"data-stat": "ties"}).text.strip()
+        team_losses = team_stat.find("td", attrs={"data-stat": "losses"}).text.strip()
+        team_goals_for = team_stat.find("td", attrs={"data-stat": "goals_for"}).text.strip()
+        team_goals_against = team_stat.find("td", attrs={"data-stat": "goals_against"}).text.strip()
+        team_goal_diff = team_stat.find("td", attrs={"data-stat": "goal_diff"}) .text.strip()
+        team_attendance_per_game = team_stat.find("td", attrs={"data-stat": "attendance_per_g"}).text.strip()
+        team_attendance_per_game = team_attendance_per_game.replace(",", "")
         
-        team_rank = team_stat.find("th", attrs={"data-stat": "rank"})
-        
-        team_games = team_stat.find("td", attrs={"data-stat": "games"})
-        
-        team_wins = team_stat.find("td", attrs={"data-stat": "wins"})
-        
-        team_ties = team_stat.find("td", attrs={"data-stat": "ties"})
-        
-        team_losses = team_stat.find("td", attrs={"data-stat": "losses"})
-        
-        team_goals_for = team_stat.find("td", attrs={"data-stat": "goals_for"})
-        
-        team_goals_against = team_stat.find("td", attrs={"data-stat": "goals_against"})
-        
-        team_goal_diff = team_stat.find("td", attrs={"data-stat": "goal_diff"}) 
-        
-        team_attendance_per_game = team_stat.find("td", attrs={"data-stat": "attendance_per_g"})
+        data, count = supabase.table("Teams").upsert({
+            "team_name": str(team_name),
+            "games": int(team_games),
+            "wins": int(team_wins),
+            "ties": int(team_ties),
+            "losses": int(team_losses),
+            "goals_for": int(team_goals_for),
+            "goals_against": int(team_goals_against),
+            "goals_diff": int(team_goal_diff),
+            "attendance_per_game": int(team_attendance_per_game),
+            "rank": int(team_rank)
+        }).execute()
     
 
 with open("./Data/mls.txt", "r") as url:
